787d5e7226cfdfd947e42642b8f68cb717962e3a
AntoineNeret <AntoineNeret@protonmail.com>
2024-10-16 09:01:14 +0200

    Création d'utilisateur

--- src/UserStory/createAccount.php
+++ src/UserStory/createAccount.php
@@ -6,6 +6,7 @@ use App\Entity\Utilisateur;
 use Doctrine\ORM\EntityManager;
 use http\Client\Curl\User;
 
+
 class createAccount
 {
     protected EntityManager $entityManager;
@@ -21,21 +22,33 @@ class createAccount
 
     //Cette méthode permettre d'éxecuter la user story
     public function execute(string $pseudo, string $mail, string $mdp): Utilisateur{
+        $accountRepository = $this->entityManager->getRepository(\App\Entity\Utilisateur::class);
         //Vérifier que les données sont présentes
         //Si tel n'est pas le cas, on va lancer une exception
-//        if ($pseudo == null && $mail == null && $mdp == null){
-
-//        }
+        if ($pseudo == null || $mail == null || $mdp == null){
+            throw new \Exception("Veuillez remplir les champs obligatoires !\n\n");
+        }
         //Vérifier si le mail est valide
         //Vérifier l'unicité du mail
         //Si tel n'est pas le cas, on lance une exception
+        if (!filter_var($mail, FILTER_VALIDATE_EMAIL)){
+            throw new \Exception("L'adresse mail n'est pas valide !\n\n");
+        }
+
+        if ($accountRepository->findOneBy(['mail' => $mail])){
+            throw new \Exception("Un compte existe déjà avec cette adresse mail !\n\n");
+        }
 
         //Vérifier si le pseudo est entre 2 et 50 caractères
         //Si tel n'est pas le cas, on lance une exception
-
+        if (strlen($pseudo) < 2 || strlen($mdp) > 50){
+            throw new \Exception("Votre pseudonyme doit avoir une longueur entre 2 et 50 caractères !\n\n");
+        }
         //Vérifier si le mot de passe est conforme aux normes de sécurité
         //Si tel n'est pas le cas, on lance une exception
-
+        if (strlen($mdp)<8){
+            throw new \Exception("Votre mot de passe doit avoir une longueur minimum de 8 caractères !\n\n");
+        }
         //Insérer dans la base de données
         //1) Hasher le mot de passe
         $mdp = password_hash($mdp,PASSWORD_ARGON2ID);
@@ -50,7 +63,7 @@ class createAccount
         $this->entityManager->flush();
 
         //5)Envoie du mail de confirmation
-        echo "Un mail de confirmation a été envoyé à l'utilisateur";
+        echo "Un mail de confirmation a été envoyé à l'utilisateur\n\n";
 
         return $utilisateur;
     }
--- test/createAccountTest.php
+++ test/createAccountTest.php
@@ -1,11 +1,56 @@
 <?php
 
 use App\Entity\Utilisateur;
+use App\UserStory\createAccount;
 use Doctrine\ORM\EntityManager;
+
+/**
+ * @var EntityManager $entityManager
+ */
+$entityManager = require_once __DIR__.'/../config/bootstrap.php';
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$utilisateur = new Utilisateur();
-$utilisateur->setMail("test@test.com");
-$utilisateur->setPseudo("test");
-$utilisateur->
+$utilisateur = new createAccount($entityManager);
+
+//On vérifie qu'un exception est lancée lorsque le pseudo n'est composé que d'un caractère
+try {
+    $utilisateur->execute("a","oui@oui.fr","0123456789");
+}catch (\Exception $e){
+    echo $e->getMessage();
+}
+
+//On vérifie qu'une exception est lancée lorsque le format du mail n'est pas valide
+try {
+    $utilisateur->execute("OuiOuiOui","non","0123456789");
+}catch (\Exception $e){
+    echo $e->getMessage();
+}
+
+//On vérifie qu'une exception est lancée lorsqu'un compte existe déjà avec l'adresse mail saisie
+try {
+    $utilisateur->execute("OuiOuiOui","oui@oui.oui","0123456789");
+}catch (\Exception $e){
+    echo $e->getMessage();
+}
+
+//On vérifie qu'une exception est lancée lorsque le mot de passe est composé de moins de 8 caractères
+try {
+    $utilisateur->execute("OuiOuiOui","non@non.non","oui");
+}catch (\Exception $e){
+    echo $e->getMessage();
+}
+
+//On vérifie qu'une exception est lancée lorsqu'aucun champ n'est rempli
+try {
+    $utilisateur->execute("","","");
+}catch (\Exception $e){
+    echo $e->getMessage();
+}
+
+//On vérifie qu'un compte est créé lorsque tout est OK
+try {
+    $utilisateur->execute("Eventuellement", "hugotlbt@gmail.com", "0123456789");
+} catch (Exception $e) {
+
+}
